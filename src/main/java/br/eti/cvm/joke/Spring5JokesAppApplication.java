3dd51b4c60ca2779db68e634886e658408626f78
Cassius Vinicius de Magalhães <cassius@cvm.eti.br>
2018-07-15 16:31:19 -0300

    Fix
    - Rename package name

--- src/main/java/br/eti/cvm/joke/Spring5JokesAppApplication.java
+++ src/main/java/br/eti/cvm/joke/Spring5JokesAppApplication.java
@@ -1,4 +1,4 @@
-package br.cvm.eti.joke;
+package br.eti.cvm.joke;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/br/eti/cvm/joke/Spring5JokesAppApplicationTests.java
+++ src/test/java/br/eti/cvm/joke/Spring5JokesAppApplicationTests.java
@@ -1,4 +1,4 @@
-package br.cvm.eti.joke;
+package br.eti.cvm.joke;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
